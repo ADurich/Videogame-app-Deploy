91628681b3c8ad66994fc0849be20ede6ff308a7
Alan <allan.dc@live.com.ar>
2022-06-17 04:32:35 -0300

    'update'

--- api/src/app.js
+++ api/src/app.js
@@ -11,13 +11,13 @@ const cors = require('cors');
 
 server.name = 'API';
 
-server.use(cors());
+server.use(cors({ credentials: true, origin: "https://videogame-app-deploy.vercel.app" }));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://videogame-app-deploy.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
