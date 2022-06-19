af3d12ca7afaacc796bccc9f58471e5e5503619c
Alan <allan.dc@live.com.ar>
2022-06-18 21:07:34 -0300

    'update'

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require('dotenv').config();
 
-conn.sync({}).then(() => {
+conn.sync({force:true}).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); 
   });
